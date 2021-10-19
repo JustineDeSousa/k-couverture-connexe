79bbfcf7791700cdd6a0956877e40e43aa6b1261
JustineDeSousa <justine.desousa6@gmail.com>
2021-10-19 18:13:18 +0200

    lecture instance

--- Instance.cpp
+++ Instance.cpp
@@ -1,7 +1,33 @@
 #include "Instance.hpp"
 
-void lecture_instance(const string instanceName ){
+void tokenize(std::string const &str, const char delim, 
+            std::vector<std::string> &out) 
+{ 
+    // construct a stream from the string 
+    std::stringstream ss(str); 
+ 
+    std::string s; 
+    while (std::getline(ss, s, delim)) { 
+        if(s != " " && s != "")
+            out.push_back(s); 
+    } 
+}
+
+
+void Instance::lecture_instance( const string instanceName, const mode mod, int gridSize ){
     string filename = "instances/" + instanceName + ".dat";
     ifstream file(filename, ios::in);
-
+    if(file){
+        if(mod == alea){
+            string line;
+            
+            while( getline(file, line) ) //Tant qu'on n'est pas à la fin, on lit
+            {
+                vector<string> tab;
+                tokenize(line, ' ', tab);
+                int nb = stoi(tab[0]);
+                targets.push_back( make_pair(stof(tab[1]), stof(tab[2])) );
+            }
+        }
+    }
 }
--- Instance.hpp
+++ Instance.hpp
@@ -3,9 +3,35 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
+#include "Solution.hpp"
+
+enum mode { alea, truncatedGrid };
 using namespace std;
 
-void lecture_instance(const string instanceName );
+class Instance
+{
+public:
+    Instance(): gridSize(0), targets(gridSize), R_capt(1), R_comm(1), k(1) {};
+    void lecture_instance( const string instanceName, const mode mod, int gridSize = 0 );
+    int grid_size(){return gridSize;};
+    vector< pair<float,float> > get_targets(){return targets;};
+    int get_R_capt(){return R_capt;};
+    int get_R_comm(){return R_comm;}
+    int get_k(){return k;};
+private:
+    int gridSize;
+    vector< pair<float,float> > targets;
+    int R_capt;
+    int R_comm;
+    int k;
+    float borne_PL;
+    Solution best_solution;
+};
+
+
 
 #endif
